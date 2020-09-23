8c023e54fc8a834cab2c4af100ef83a31636923c
Re <refanopradana@gmail.com>
2020-09-23 13:24:18 +0700

    Final Version of Markdown

--- markdown/src/App.js
+++ markdown/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import marked from 'marked';
+import ReactMarkdown from "react-markdown";
 
 export default function App() {
-  return (
-    <div className="app">
-      <textarea />
+    const [markDown, setMarkDown] = useState('# sup');
 
-      <div className="preview" />
+    function handleChange(event) {
+        setMarkDown(event.target.value)
+    }
+
+    return (
+    <div className="app">
+      <textarea onChange={handleChange} value={markDown}/>
+{/*
+        <div className="preview" dangerouslySetInnerHTML={{__html: marked(markDown)}}/>*/}
+        <ReactMarkdown className="preview" source={markDown}/>
     </div>
   );
 }
