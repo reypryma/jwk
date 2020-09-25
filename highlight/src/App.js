64dca9303702e546a3a3540e05732c6825b0a317
Re <refanopradana@gmail.com>
2020-09-26 04:07:23 +0700

    1 Tab highlight, ease change style to each state using ease all css, set state for left:0 to highligth. The highlight will move useing function that capture event especially layerX. Call onMoueseOut, onMouseMove

--- highlight/src/App.js
+++ highlight/src/App.js
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [highlightStyle, setHighlightStyle] = useState({
+    left:0
+  });
+
+
+  function moveHighLight(e) {
+    setHighlightStyle({
+      left: e.nativeEvent.layerX -150,
+    })
+  }
+
+  function hideHighLight(e) {
+    setHighlightStyle({
+      opacity:0,
+      left: e.nativeEvent.layerX -150,
+    })
+  }
+
   return (
     <div className="app">
       <div className="browser">
-        <div className="tabs">
-          <div className="tab">
+        <div className="tabs" >
+          <div className="tab"
+               onMouseOut={hideHighLight}
+               onMouseMove={moveHighLight}>
+            <div className="highlight" style={highlightStyle}/>
             <a>Home</a>
           </div>
-          <div className="tab">
+          <div className="tab"
+               onMouseOut={hideHighLight}
+               onMouseMove={moveHighLight}>
+            <div className="highlight" style={highlightStyle}/>
             <a>About</a>
           </div>
-          <div className="tab">
+          <div className="tab" onMouseOut={hideHighLight} onMouseMove={moveHighLight}>
+            <div className="highlight" style={highlightStyle}/>
             <a>Features</a>
           </div>
         </div>
